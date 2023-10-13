ce6ccd4034052f0e5666dc2ca01e2c3332ad3125
kiran1842000 <145153464+kiran1842000@users.noreply.github.com>
2023-10-13 19:42:07 -0400

    Add files via upload
    lab 5 uploaded

---
+++ first/first/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first
+//
+//  Created by IS on 2023-10-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
